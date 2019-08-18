35e8ce9ee26d401289e525716c924f80d69720bf
4330509 <amalan_akash@hotmail.com>
2019-08-18 23:02:17 +0200

    updated ConversationHandler

--- src/ConversationHandler.java
+++ src/ConversationHandler.java
@@ -4,23 +4,51 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+
 public class ConversationHandler extends Thread {
 
     Socket socket;
     BufferedReader in;
     PrintWriter out;
-
+    String name;
 
     public ConversationHandler(Socket socket) throws IOException {
         this.socket = socket;
-
     }
+
 
     public void run() {
 
         try {
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new PrintWriter(socket.getOutputStream(), true);
+
+            int count = 0;
+            while (true) {
+                if (count > 0) {
+                    out.println("NAMEALREADYEXISTS");
+                } else {
+                    out.println("NAMEREQUIRED");
+                }
+                name = in.readLine();
+
+                if (name == null) {
+                    return;
+                }
+
+                if (!Server.userNames.contains(name)) {
+                    Server.userNames.add(name);
+                    break;
+                }
+
+                count++;
+
+            }
+            out.println("NAMEACCEPTED");
+            Server.printWriters.add(out);
+
+
+
         } catch (Exception e) {
             System.out.println(e);
         }
--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,12 @@
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server {
+    static ArrayList<String> userNames = new ArrayList<String>();
+    static ArrayList<PrintWriter> printWriters = new ArrayList<PrintWriter>();
+
 
     public static void main(String[] args) throws Exception {
         System.out.println("Waiting for clients ...");
